9c99e44bc491bc3fc077e34ad8beee004a14e264
Mahsa-For <mforghanitehrani@gmail.com>
2026-07-17 16:47:32 +0200

    update colors and home page

--- Boxology-Interface/src/data/shape.ts
+++ Boxology-Interface/src/data/shape.ts
@@ -2,28 +2,32 @@ import { Label } from "@mui/icons-material";
 
 export const shapes: ShapeDefinition[] = [
   // Data & Information
-  { name: 'Symbol', label: 'id-symbol', shape: 'Rectangle', color: '#ccffccff', stroke: '#218721ff', group: 'Data & Information' },
-  { name: 'Data', label: 'id-data', shape: 'Rectangle', color: '#b7eaffff', stroke: '#1E5F8B', group: 'Data & Information' },
+  { name: 'Symbol', label: 'id-symbol', shape: 'Rectangle', color: '#d1f0c9ff', stroke: '#218721ff', group: 'Data & Information' },
+  { name: 'Data', label: 'id-data', shape: 'Rectangle', color: '#c4e8ffff', stroke: '#1E5F8B', group: 'Data & Information' },
   //{ name: 'artifact', label: 'id-artifact', shape: 'Rectangle', color: '#99e5e1ff', stroke: '#0D5F5C', group: 'Data & Information' },
 
   // Actors & Entities
-  { name: 'Actor', label: 'id-actor', shape: 'Triangle', color: '#f8ce92ff', stroke: '#000000ff', group: 'Actors & Entities' },
+  { name: 'Actor', label: 'id-actor', shape: 'Triangle', color: '#ffdcabff', stroke: '#9c6921', group: 'Actors & Entities' },
 
   // AI & Models
-  { name: 'Model', label: 'id-model', shape: 'Hexagon', color: '#f4ccf4ff', stroke: '#8B4F8B', group: 'AI & Models' },
+  { name: 'Model', label: 'id-model', shape: 'Hexagon', color: '#edd3f3ff', stroke: '#8B4F8B', group: 'AI & Models' },
   //{ name: 'SemanticModel', label: 'id-SemanticModel', shape: 'Hexagon', color: '#f4ccf4ff', stroke: '#8B4F8B', group: 'AI & Models' },
   //{ name: 'StatisticalModel', label: 'id-StatisticalModel', shape: 'Hexagon', color: '#f4ccf4ff', stroke: '#8B4F8B', group: 'AI & Models' },
 
   // Processes & Actions
-  { name: 'Transform', label: 'id-transform', shape: 'RoundedRectangle',color: '#fbf2a2ff', stroke: '#B8A600', group: 'Processes & Actions' },
-  { name: 'Deduce', label: 'id-deduce', shape: 'RoundedRectangle', color: '#FFC4C4', stroke: '#4c003bff', group: 'Processes & Actions' },
-  { name: 'Train', label: 'id-training', shape: 'RoundedRectangle', color: '#FFA07A', stroke: '#CD5C5C', group: 'Processes & Actions' },
-  { name: 'Engineer', label: 'id-engineering', shape: 'RoundedRectangle', color: '#F5E5E1', stroke: '#C1307A', group: 'Processes & Actions' },
+  { name: 'Transform', label: 'id-transform', shape: 'RoundedRectangle',color: '#fbf1b0ff', stroke: '#B8A600', group: 'Processes & Actions' },
+  { name: 'Deduce', label: 'id-deduce', shape: 'RoundedRectangle', color: '#ffcdcd', stroke: '#4c003bff', group: 'Processes & Actions' },
+  { name: 'Deduce', label: 'nn:deduce', shape: 'RoundedRectangle', color: '#ffcdcd', stroke: '#b60077', group: 'Processes & Actions' },
+  { name: 'Deduce', label: 'ss:deduce', shape: 'RoundedRectangle', color: '#ffcdcd', stroke: '#9c3a5f', group: 'Processes & Actions' },
+  { name: 'Deduce', label: 'co:deduce', shape: 'RoundedRectangle', color: '#ffcdcd', stroke: '#c27ba0', group: 'Processes & Actions' },
+  { name: 'Deduce', label: 'infe:deduce', shape: 'RoundedRectangle', color: '#ffcdcd', stroke: '#a64d79', group: 'Processes & Actions' },
+  { name: 'Train', label: 'id-training', shape: 'RoundedRectangle', color: '#ffc2a0', stroke: '#c05f30', group: 'Processes & Actions' },
+  { name: 'Engineer', label: 'id-engineering', shape: 'RoundedRectangle', color: '#f6d6cf', stroke: '#b85068', group: 'Processes & Actions' },
   //{ name: 'generate', label: 'id-generate', shape: 'RoundedRectangle', color: '#f067acff', stroke: '#C1307A', group: 'Processes & Actions' },
   //{ name: 'embedding', label: 'id-embedding', shape: 'RoundedRectangle', borderRadius: '45px', color: '#5a562fff', stroke: '#B8A600', group: 'Processes & Actions' },
 
   // Documentation
-  { name: 'comment', label: 'id-comment', shape: 'Rectangle', color: '#F5F5DC', stroke: '#A9A9A9', group: 'Documentation' },
+  { name: 'comment', label: 'id-comment', shape: 'Rectangle', color: '#f2f0d6', stroke: '#a79c66', group: 'Documentation' },
 ];
 
 // ADD: Type definitions for each shape
